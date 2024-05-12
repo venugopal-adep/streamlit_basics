2fb1ef7ffb3db1228c84cb4c7341e682fb54534b
venugopal-adep <92349362+venugopal-adep@users.noreply.github.com>
2024-05-12 11:19:09 +0530

    Update 01_Welcome_message.py

--- 01_Welcome_message.py
+++ 01_Welcome_message.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title('Welcome to Streamlit!')
 st.write('This is your first Streamlit app. Exciting, right?')
+
+#Exercise : Modify the code snippet to add a subheader below the title that says "Welcome to your first Streamlit app!"
